b1c80cb041eca5de70c8961d3598380a29c1d754
sakhawatali <msakhawatali131@gmail.com>
2025-11-11 12:53:12 +0500

    Solved problem xyz

---
+++ Binary-Search/Search_a_2D_Matrix.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+# Create a NumPy array (matrix)
+matrix_np = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+
+# Get the dimensions (rows, columns)
+rows, cols = matrix_np.shape
+
+print(f"Number of rows: {rows}")
+print(f"Number of columns: {cols}")
+print(f"Shape of the matrix: {matrix_np.shape}")
