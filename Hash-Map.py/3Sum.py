01b0c6ff78a17d7aa8ecc3fd6d65822161e4aceb
sakhawatali <msakhawatali131@gmail.com>
2025-11-25 13:11:17 +0500

    adding leetcode problem 3Sum solution

---
+++ Hash-Map.py/3Sum.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def threeSum(self, nums):
+
+        n = len(nums)
+        ans = []
+        nums.sort()
+        for i in range(n):
+            if i > 0 and nums[i] == nums[i-1]:
+                continue  
+            j = i + 1
+            k = n - 1
+            while j < k:
+                total = nums[i] + nums[j] + nums[k] 
+                if total < 0:
+                    j += 1
+                elif total > 0:
+                    k -= 1
+                else:
+                    ans.append([nums[i], nums[j], nums[k]])  
+                    j += 1
+                    k -= 1
+                    while j < k and nums[j] == nums[j-1]:
+                        j += 1
+        return ans
