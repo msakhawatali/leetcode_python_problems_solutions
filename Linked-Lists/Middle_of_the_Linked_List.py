fb45f2d07572b3759b3e1194396e83e57ddbbd93
Sakhawat Ali <msakhawatali131@gmail.com>
2026-01-24 16:24:27 +0500

    adding middle of the linked list problem solution

--- Linked-Lists/Middle_of_the_Linked_List.py
+++ Linked-Lists/Middle_of_the_Linked_List.py
@@ -5,18 +5,11 @@
 #         self.next = next
 class Solution(object):
     def middleNode(self, head):
-        """
-        :type head: Optional[ListNode]
-        :rtype: Optional[ListNode]
-        """
-        curr = head 
-        l = 0
-        while curr != None:
-            curr = curr.next
-            l += 1
-        
-        curr = head 
-        for i in range(l//2):
-            curr = curr.next
-        
-        return curr
+        slow = head
+        fast = head
+
+        while fast != None and fast.next != None:
+            slow = slow.next
+            fast = fast.next.next
+
+        return slow
