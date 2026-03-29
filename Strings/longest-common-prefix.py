6db67e2d7cf9f140da7e3a2985f338fb4e463045
sakhawat ali <msakhawatali131@gmail.com>
2026-03-29 14:06:01 +0500

    adding longest common prefix solution

--- Strings/longest-common-prefix.py
+++ Strings/longest-common-prefix.py
@@ -1,11 +1,14 @@
-class Solution:
-  def longestCommonPrefix(self, strs: list[str]) -> str:
-    if not strs:
-      return ''
-
-    for i in range(len(strs[0])):
-      for j in range(1, len(strs)):
-        if i == len(strs[j]) or strs[j][i] != strs[0][i]:
-          return strs[0][:i]
-
-    return strs[0]
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        strs.sort()
+        common_prefix = ''
+        n = 0
+        for i in strs[0]:
+            if i == strs[len(strs)-1][n]:
+                common_prefix += i
+                n += 1
+                continue
+            else:
+                break
+        return common_prefix
+    
