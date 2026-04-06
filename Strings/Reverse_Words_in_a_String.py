1a125287f4f9b3813606b578c926ceddbe755157
sakhawat ali <msakhawatali131@gmail.com>
2026-04-06 12:51:28 +0500

    adding reverse words in a sreing problem solution

--- Strings/Reverse_Words_in_a_String.py
+++ Strings/Reverse_Words_in_a_String.py
@@ -1,23 +1,5 @@
 class Solution(object):
     def reverseWords(self, s):
-
-        n = len(s)
-        ans = ""
-        s = s[::-1]
-        i = 0
-
-        while i < n:
-            while i < n and s[i] == " ":
-                i += 1
-
-            word = ""
-            while i < n and s[i] != " ":
-                word += s[i]
-                i += 1
-
-            if word:
-                if ans:
-                    ans += " "
-                ans += word[::-1]
-
-        return ans
+        w = s.split()
+        w.reverse()
+        return " ".join(w)
