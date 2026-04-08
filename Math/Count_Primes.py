101141d5d8338914554d90fd02bf0e5f81080b90
sakhawat ali <msakhawatali131@gmail.com>
2026-04-08 13:53:00 +0500

    adding count primes problem solution

--- Math/Count_Primes.py
+++ Math/Count_Primes.py
@@ -1,4 +1,12 @@
-n = 10
-for i in range(n):
-    if n >=2:
-        pass
+class Solution(object):
+    def countPrimes(self, n):
+        if n == 0: print(0)
+        if n == 1: print(0)
+        is_prime = [True] * (n+1)
+        count = 0
+        for i in range(2, n):
+            if is_prime[i] == True:
+                count += 1
+                for j in range(i*2, n, i):
+                    is_prime[j] = False
+        return count
