d82b1b48d375881706c00af5938e21e20a9558eb
sakhawat ali <msakhawatali131@gmail.com>
2026-04-11 23:44:30 +0500

    adding search a 2D matrix problem solution

--- Arrays/Matrix-Problems/Search_a_2D_Matrix.py
+++ Arrays/Matrix-Problems/Search_a_2D_Matrix.py
@@ -1,31 +1,22 @@
-import numpy as np
 class Solution(object):
     def searchMatrix(self, matrix, target):
+        m = len(matrix)
+        n = len(matrix[0])
 
-        def SearchInRow(matrix, target, midRow):
-            matrix_np = np.array(matrix)
-            rows, cols = matrix_np.shape
-            st, end = 0, cols - 1
-            while st <= end:
-                mid = st + ((end - st)/2)
-                if target == matrix[midRow][mid]:
-                    return True
-                elif target > matrix[midRow][mid]:
-                    st = mid + 1
-                else:
-                    end = mid - 1
-            return False
+        start = 0
+        end = m*n-1
 
-        matrix_np = np.array(matrix)
-        rows, cols = matrix_np.shape
-        startRow, endRow = 0, rows - 1
-        while startRow <= endRow:
-            midRow = startRow + ((endRow - startRow)/2)
-            if target >= matrix[midRow][0] and target <= matrix[midRow][cols - 1]:
-                return SearchInRow(matrix, target, midRow)
-            elif target >= matrix[midRow][cols - 1]:
-                startRow = midRow + 1
-            else : 
-                endRow = midRow - 1
+        while start <= end:
+            mid = start + (end-start)//2
+            row = mid//n
+            
+            col = int(mid%n)
+            print(col)
+            if matrix[row][col] > target:
+                end = mid - 1
+            elif matrix[row][col] < target:
+                start = mid + 1
+            else:
+                return True
         return False
-        
+            
