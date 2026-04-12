db293b99f9f4e1e85f313f114cc7100549fcce13
sakhawat ali <msakhawatali131@gmail.com>
2026-04-12 12:34:13 +0500

    adding search a 2D matrix II problem solution

--- Arrays/Matrix-Problems/Search_a_2D_Matrix_II.py
+++ Arrays/Matrix-Problems/Search_a_2D_Matrix_II.py
@@ -1,15 +1,20 @@
-import numpy as np
 class Solution(object):
-    def searchMatrix(self, mat, target):
-
-        matrix = np.array(mat)
-        rows, cols = matrix.shape
-        r, c = 0, cols-1
-        while r < rows and c >= 0:
-            if target == mat[r][c]:
-                return True
-            elif target < mat[r][c]:
-                c -= 1
+    def searchMatrix(self, matrix, target):
+        m = len(matrix)   
+        n = len(matrix[0])  
+        col = 0
+        while col <= m-1:
+            if matrix[col][0] <= target:
+                start = 0
+                end = n-1
+                while start <= end:
+                    mid = start + (end - start) // 2
+                    if matrix[col][mid] < target:
+                        start = mid + 1
+                    elif matrix[col][mid] > target:
+                        end = mid - 1
+                    else: 
+                        return True
+                col += 1
             else:
-                r += 1
-        return False
+                return False
