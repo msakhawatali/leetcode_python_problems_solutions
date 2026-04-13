c03c8d84cf10aa2392efe0ee50cacfbeaa18774e
sakhawat ali <msakhawatali131@gmail.com>
2026-04-13 15:06:38 +0500

    adding plus one problem solution

--- Math/Plus_One.py
+++ Math/Plus_One.py
@@ -1,20 +1,13 @@
 class Solution(object):
     def plusOne(self, digits):
-        """
-        :type digits: List[int]
-        :rtype: List[int]
-        """
-        digits = digits[::-1]
-        one , i = 1, 0
-        while one:
-            if i < len(digits):
-                if digits[i] == 9:
-                    digits[i] = 0
-                else:
-                    digits[i] += 1
-                    one = 0
-            else:
-                digits.append(1)
-                one = 0
-            i += 1
-        return digits[::-1]
+        n = len(digits)
+        for i in range(n - 1, -1, -1):
+            if digits[i] < 9:
+                digits[i] += 1
+                return digits
+            
+            digits[i] = 0
+            
+        return [1] + digits
+        
+
