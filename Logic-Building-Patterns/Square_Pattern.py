fe76b9b946140a84b8706178b25e6642c63151b5
Sakhawat Ali <msakhawatali131@gmail.com>
2026-07-08 12:10:28 +0500

    adding square pattern solving

--- Logic-Building-Patterns/Square_Pattern.py
+++ Logic-Building-Patterns/Square_Pattern.py
@@ -1,6 +1,20 @@
+# 1 2 3 4
+# 1 2 3 4
+# 1 2 3 4
 n = int(input("Enter a number for pattern: "))
-
 for i in range(n):
     for j in range(1, n+1):
         print(j, end=" ")
+    print()
+
+
+# 1 2 3 
+# 4 5 6 
+# 7 8 9 
+n = int(input("Enter a number for pattern: "))
+num = 1
+for i in range(n):
+    for j in range(n):
+        print(num, end=" ")
+        num += 1
     print()
