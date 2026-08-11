82f77917b81c9b10df500ce1ef81e0a55bbe2da7
Sakhawat Ali <msakhawatali131@gmail.com>
2026-08-11 13:18:07 +0500

    adding leetcode problem sqrtd(x) new solution

--- Binary-Search/Sqrt(x).py
+++ Binary-Search/Sqrt(x).py
@@ -1,13 +1,12 @@
 class Solution(object):
     def mySqrt(self, x):
-
-        st , end = 0 , x
+        st, end = 0, x
         res = 0
         while st <= end:
             m = st + ((end - st) // 2)
-            if m**2 > x:
+            if m * m > x:
                 end = m - 1
-            elif m**2 < x:
+            elif m * m < x:
                 st = m + 1
                 res = m
             else:
