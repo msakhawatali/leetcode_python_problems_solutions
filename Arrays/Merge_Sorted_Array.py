54cb6d6e839aefa7d26d273a5bb82e54e40ccf56
Sakhawat Ali <msakhawatali131@gmail.com>
2026-08-14 17:49:23 +0500

    update leetcode problem marge sorted array solution

--- Arrays/Merge_Sorted_Array.py
+++ Arrays/Merge_Sorted_Array.py
@@ -1,18 +1,13 @@
 class Solution(object):
     def merge(self, nums1, m, nums2, n):
-        idx = m+n-1
-        i = m-1
-        j = n-1
-        while i >= 0 and j >= 0:
-            if nums1[i] >= nums2[j]:
-                nums1[idx] = nums1[i]
-                idx -= 1
-                i -= 1 
+        write_index = m + n - 1
+        nums1_index = m - 1
+        nums2_index = n - 1
+        while nums2_index >= 0:
+            if nums1_index >= 0 and nums1[nums1_index] > nums2[nums2_index]:
+                nums1[write_index] = nums1[nums1_index]
+                nums1_index -= 1
             else:
-                nums1[idx] = nums2[j]
-                idx -= 1
-                j -= 1
-        while j >= 0:
-            nums1[idx] = nums2[j]
-            idx -= 1
-            j -= 1
+                nums1[write_index] = nums2[nums2_index]
+                nums2_index -= 1
+            write_index -= 1
