bb5e66777e4d7256e9d20152452f644d408b6c7b
Sakhawat Ali <msakhawatali131@gmail.com>
2026-09-10 14:28:35 +0500

    update Valid Anagram solution

--- sorting/Valid_Anagram.py
+++
@@ -1,8 +0,0 @@
-class Solution(object):
-    def isAnagram(self, s, t):
-        s_sorted = "".join(sorted(s))
-        t_sorted = "".join(sorted(t))
-        if s_sorted == t_sorted:
-            return True
-        else:
-            return False
